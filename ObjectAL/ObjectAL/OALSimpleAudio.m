f737236b4bb6873953a4625e0f2908cda9d43648
Karl Stenerud <kstenerud@gmail.com>
2013-11-20 12:34:24 -0800

    Destroy the OALSimpleAudio shared instance on error. Otherwise SynthesizeSingleton keeps the broken instance around.

--- ObjectAL/ObjectAL/OALSimpleAudio.m
+++ ObjectAL/ObjectAL/OALSimpleAudio.m
@@ -137,6 +137,7 @@ SYNTHESIZE_SINGLETON_FOR_CLASS(OALSimpleAudio);
 	return self;
 
 initFailed:
+    [[self class] purgeSharedInstance];
     as_release(self);
     return nil;
 }
@@ -163,6 +164,7 @@ initFailed:
 	return self;
 
 initFailed:
+    [[self class] purgeSharedInstance];
     as_release(self);
     return nil;
 }
